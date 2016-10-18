6f6c0e5c92df8670d9da688c892ce74105289cea
Kevin Hobbs <kevin21921@gmail.com>
2016-10-18 12:36:17 -0400

    Added brand styling, create actions and validations.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,23 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
+
+		if @contact.save
+			flash[:success] = 'Thank you for your feedback!'
+			redirect_to new_contact_path
+		else
+			flash[:danger] = 'Error Occured. Message Not Sent. Try Again!'
+			redirect_to new_contact_path
+		end
+
 	end
 
+	def index
+	end
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end
 end
